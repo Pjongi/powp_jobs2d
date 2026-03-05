f785098c697189314aeef4bebc11a3050cdfdc8f
pjongi <blutarchone@gmail.com>
2026-03-05 11:16:40 +0100

    3.2 naprawa "wzorem z testu „Figure Joe 1” na pewno nie miała być parasolka"

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -28,10 +28,12 @@ public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 		line.setEndCoordinates(x, y);
 
 		drawLine(line);
+
+		setPosition(x, y);
 	}
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "DrawerAdapter";
 	}
 }
